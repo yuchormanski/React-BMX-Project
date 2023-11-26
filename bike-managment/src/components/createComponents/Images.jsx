f61085ae40f359464cfd7a4621c34edab1163b4c
yuchormanski <yuchormanski@gmail.com>
2023-11-26 13:48:52 +0200

    added images modal

--- bike-managment/src/components/createComponents/Images.jsx
+++ bike-managment/src/components/createComponents/Images.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import styles from "./Images.module.css";
 
 function Images({ imgArray }) {
   const [arr, setArr] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [currentImg, setCurrentImg] = useState("");
 
   useEffect(
     function () {
@@ -15,12 +18,43 @@ function Images({ imgArray }) {
     [imgArray]
   );
 
-  if (arr) return <></>;
+  function onClickHandler(img) {
+    setCurrentImg(img);
+    setModal(true);
+  }
+
+  function closeModalHandler() {
+    setModal(false);
+    setCurrentImg("");
+  }
+
+  if (!arr) return <></>;
 
   return (
-    <div className={styles.imgBox}>
-      <p>jfdhsl</p>
-    </div>
+    <>
+      {modal && (
+        <div className={styles.modal}>
+          <div className={styles.topContent}>
+            <button className={styles.closeBtn} onClick={closeModalHandler}>
+              <ion-icon name="close-outline"></ion-icon>
+            </button>
+            <img src={currentImg} alt="Frame Image" />
+          </div>
+        </div>
+      )}
+
+      <div className={styles.imgBox}>
+        {arr.length > 0 &&
+          arr.map((x, i) => (
+            <img
+              src={x}
+              alt="Frame image"
+              key={i}
+              onClick={() => onClickHandler(x)}
+            />
+          ))}
+      </div>
+    </>
   );
 }
 
