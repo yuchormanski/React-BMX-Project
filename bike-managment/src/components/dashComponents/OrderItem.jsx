cb90521d703ae1bd57a14c29b51ec8c58c2ae8b0
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-12-03 22:41:05 +0200

    fixed wrong id

--- bike-managment/src/components/dashComponents/OrderItem.jsx
+++ bike-managment/src/components/dashComponents/OrderItem.jsx
@@ -12,6 +12,7 @@ function OrderItem({ product, onBtnHandler, orderId }) {
   const [index, setIndex] = useState(null);
   const [item, setItem] = useState("");
   const [id, setId] = useState("");
+  const [firstCall, setFirstCall] = useState(false);
   const [isDone, setIsDone] = useState(false);
   const [loading, setLoading] = useState(false);
 
@@ -19,11 +20,11 @@ function OrderItem({ product, onBtnHandler, orderId }) {
 
   useEffect(function () {
     const abortController = new AbortController();
+    setId(orderId);
 
     if (user.department === "Frames") {
       setItem(product.orderStates[0]);
       setIndex(0);
-      setId(product.id);
     }
     if (user.department === "Wheels") {
       setItem(product.orderStates[1]);
@@ -39,7 +40,7 @@ function OrderItem({ product, onBtnHandler, orderId }) {
 
   useEffect(
     function () {
-      if (item.isProduced) {
+      if (item.isProduced || firstCall) {
         // make copy on original object
         newProduct = { ...product };
 
@@ -53,11 +54,12 @@ function OrderItem({ product, onBtnHandler, orderId }) {
         onBtnHandler();
       }
     },
-    [isDone]
+    [isDone, firstCall]
   );
 
   function onButtonClick() {
-    let currentDate = new Date();
+    let currentDate = new Date().getTime();
+    // let currentDate = new Date();
     // console.log("in btn");
 
     if (item.startedTime === "" && item.finishedTime === "") {
@@ -66,10 +68,10 @@ function OrderItem({ product, onBtnHandler, orderId }) {
         startedTime: currentDate,
         nameOfEmplоyeeProducedThePart: `${user.firstName} ${user.lastName}`,
       });
-    }
-    if (item.startedTime !== "" && item.finishedTime === "") {
+      setFirstCall(!firstCall);
+    } else if (item.startedTime !== "" && item.finishedTime === "") {
       setItem({ ...item, finishedTime: currentDate, isProduced: true });
-      setIsDone(true);
+      setIsDone(!isDone);
     }
   }
 
@@ -104,16 +106,11 @@ function OrderItem({ product, onBtnHandler, orderId }) {
         <div className={styles.info}>
           <p className={styles.model}>
             <span>Started on: </span>
-            {item.startedTime &&
-              item.startedTime
-                .toLocaleString()
-                .split(", ")[0]
-                .replaceAll("/", ".")}
+            {item.startedTime && new Date(item.startedTime).toDateString()}
           </p>
           <p className={styles.model}>
             <span>Finished on: </span>
-            {item.finishedTime &&
-              item.finishedTime.toLocaleString().split(", ")[0]}
+            {item.finishedTime && new Date(item.finishedTime).toDateString()}
           </p>
           {/* <p className={styles.partId}>ID# {item.partId}</p> */}
           <p className={styles.partId}>ID# {orderId + "-" + item.partId}</p>
@@ -144,13 +141,3 @@ function OrderItem({ product, onBtnHandler, orderId }) {
 }
 
 export default OrderItem;
-
-// {
-//   "partId": 2,
-//   "partType": "Wheel",
-//   "partModel": "Wheel of the Year",
-//   "nameOfEmplоyeeProducedThePart": " ",
-//   "isProduced": false,
-//   "startedTime": "",
-//   "finishedTime": ""
-// }
