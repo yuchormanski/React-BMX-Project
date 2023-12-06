d3ce0244be30aef14fe7ff68b2c8c503cb70a621
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-12-06 11:43:41 +0200

    added pagination

--- bike-managment/src/components/dashComponents/managerComponents/ManagerOrders.jsx
+++ bike-managment/src/components/dashComponents/managerComponents/ManagerOrders.jsx
@@ -4,35 +4,76 @@ import styles from "./ManagerOrders.module.css";
 import { getOrdersList } from "../../../bikeServices/service.js";
 import Order from "./Order.jsx";
 import Paginator from "../../Paginator.jsx";
+import { usePagination } from "../../../customHooks/usePaginationArray.js";
+import { environment } from "../../../environments/environment_dev.js";
 
 function ManagerOrders() {
   const [orders, setOrders] = useState({});
+  const [dataReceived, setDataReceived] = useState([]);
+  const [length, setLength] = useState(0);
   const [page, setPage] = useState(1);
+
+  // useEffect(function () {
+  //   const abortController = new AbortController();
+
+  //   async function getOrders() {
+  //     const orders = await getOrdersList();
+  //     orders.sort((a, b) => a.createdAt - b.createdAt);
+  //     setOrders(orders);
+  //   }
+  //   getOrders();
+
+  //   return () => abortController.abort();
+  // }, []);
+
+  // function onOrdersChange(newData) {
+  //   setOrders(newData);
+  // }
+
+  const path = environment.orders;
+  const itemPerPage = 2;
+  // console.log(orders);
+
+  const data = usePagination(path);
+  const dataArray = [];
 
   useEffect(function () {
     const abortController = new AbortController();
-
-    async function getOrders() {
-      const orders = await getOrdersList();
-      orders.sort((a, b) => a.createdAt - b.createdAt);
-      setOrders(orders);
+    async function getOrdersPagination() {
+      const list = await data;
+      setLength(list.length);
+      setDataReceived(list);
     }
-    getOrders();
+    getOrdersPagination();
 
     return () => abortController.abort();
   }, []);
+
+  useEffect(
+    function () {
+      if (dataReceived.length > 0) {
+        for (let i = 0; i < dataReceived.length; i += itemPerPage) {
+          const chunk = dataReceived.slice(i, i + itemPerPage);
+          dataArray.push(chunk);
+        }
+        setOrders(dataArray[page - 1]);
+      }
+    },
+    [page]
+  );
 
   function onOrdersChange(newData) {
     setOrders(newData);
   }
 
   function handlePage(page) {
-    console.log(page);
+    // console.log(page);
     setPage(page);
   }
 
-  if (orders.length === 0) return <h2>There is no orders in this category</h2>;
-  if (orders.length > 0)
+  if (orders && orders.length === 0)
+    return <h2>There is no orders in this category</h2>;
+  if (orders && orders.length > 0)
     return (
       <>
         <h2 className={styles.dashHeading}>Orders in sequence</h2>
@@ -44,10 +85,9 @@ function ManagerOrders() {
             ))}
           </div>
           <Paginator
-            // pages={orders.length}
             page={page}
-            pages={30}
-            countOnPage={4}
+            pages={length}
+            countOnPage={itemPerPage}
             size={24}
             fontSize={1.6}
             // bgColor=""
